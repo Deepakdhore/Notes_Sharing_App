8ea2853789d490ee293dd5628734a9a22c8f63da
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2025-09-05 11:39:04 +0530

    Revert "Implemented Router"

--- Notebook/src/Pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Registerform from '../Components/auth/Registerform'
-function RegisterPage() {
-  return (
-    <div><Registerform/></div>
-  )
-}
-
-export default RegisterPage
--- Notebook/src/main.jsx
+++ Notebook/src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import InputField from './Components/InputFeild/Inputfield.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import InputField from './Components/Inputfield.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
